fe8f1aac23bc620876c9288be9996c8fd9b6e8c4
D4ng <53222130+hainamzsd@users.noreply.github.com>
2023-06-30 14:33:01 +0700

    Add project files.

---
+++ project/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using project.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<NorthwindContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "DefaultCookie";
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ project/SessionExtensions/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace project.SessionExtensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetSession<T>(this ISession session, string key, T value)
+        {   
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T? GetSession<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+
+    }
+}
